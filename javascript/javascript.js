73c3e607d9e972da30a69ff57460b74b784b8b34
polarvogel <github@polarvogel.de>
2018-03-31 19:59:23 +0200

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -15,8 +15,10 @@ function calculateButton() {
   var input = parseInt(document.getElementById("oldNumber").value.replace(/\s/g,''));
   var output = getCalculatedNumber(input)
   document.getElementById("newNumber").innerHTML = output;  
+  var bc_height = document.getElementById("bc_height").value.replace(/\s/g,'');
+  var bc_width = document.getElementById("bc_width").value.replace(/\s/g,'');
   // https://github.com/lindell/JsBarcode
-  JsBarcode("#barcode", output, {format: "ITF", displayValue:false, width:2, height:100});
+  JsBarcode("#barcode", output, {format: "ITF", displayValue:false, width:bc_width, height:bc_height});
   var dataURL = document.getElementById('barcode').toDataURL();
   document.getElementById('canvasImg').src = dataURL;
   document.getElementById('imageLink').href= dataURL;  
